498091788865f70ef2432d3def1ccd44bc093128
mahe <mahesh9415@gmail.com>
2017-07-24 21:39:14 +0530

    initial commit

---
+++ src/components/dashboard.vue
@@ -0,0 +1,297 @@
+<template>
+      <v-layout row wrap justify-center> 
+      <div v-for="post in posts">
+      <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          :src="post.thump"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">{{post.title}}</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+          {{post.description}}
+            <!-- <span class="grey--text">Number 10</span><br> -->
+            <!-- <span>Whitehaven Beach</span><br> -->
+            <!-- <span>Whitsunday Island, Whitsunday Islands</span> -->
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <a :href="post.url" target="_blank">
+          <v-btn flat class="orange--text">  Explore
+          </v-btn></a>
+        </v-card-actions>
+      </v-card>
+     <!--     <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+          <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>    <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>    <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>    <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>    <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>    <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>    
+      <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card class="mx-5 my-3">
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/img/te.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Top 10 Australian beaches</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card> -->
+      </div>
+  </v-layout>
+</template>
+<script>
+export default{
+name:'dashboard',
+ data () {
+ 	return{
+ 		posts:[]
+ 	}
+ },
+ mounted(){
+ 	const post={
+ 		title:'A new blog post',
+ 		description:'BentoBox, the website builder for restauranteurs, cooks up $4.8M Series A',
+ 		thump:'/static/img/te.jpg',
+ 		url:'https://vookmark.co'
+ 	}
+ 	this.posts.push(post);
+ 	console.log(this.posts)
+ 	console.log(post);
+ }
+}
+</script>
+<style>
+	#example-1 {
+    color: #fff;
+    text-align: center;
+  }
+  #example-1 .flex {
+    margin-bottom: 16px;
+  }
+  a{
+  	text-decoration: none;
+  }
+</style>
